b9eea7a02a866d9840d164bf789a17a304d6d222
dipolack <pol22007@byui.edu>
2023-01-07 01:25:02 -0500

    week 1 assignment

---
+++ week1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Great, your name is {last}, {first} {last}.");
+    }
+}
