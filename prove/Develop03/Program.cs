b1d289eeaa5d297c343317252680b4a6e6b20712
dipolack <pol22007@byui.edu>
2023-02-13 06:13:59 -0500

    working on scripture class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,6 +3,7 @@ using System;
 class Program
 {
     static void Main()
+
     {
         //Scripture class, remember to put on separate file//
         Scripture scripture = new Scripture ("John 3:16: For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Scripture 
+{
+    private string reference;
+    private string text;
+
+    private List <string> hiddenWords;
+
+    public Scripture (string reference, string text)
+
+    {
+        this.reference = reference;
+        this.text = text;
+        this.hiddenWords = new List<string> ();
+        
+    }
+
+    public void DisplayScripture ()
+
+    {
+
+        //write line
+        //string words
+        //hiddenwords
+        //if words are hidden console write
+        //generate random
+        //make sure to run program
+
+    }
+}
