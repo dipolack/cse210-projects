643247000f02251e42b4ebec28c67bf83352b581
dipolack <pol22007@byui.edu>
2023-02-14 00:48:01 -0500

    trying to fix scripture string

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,41 +4,42 @@ using System.Linq;
 class Program
 {
     static void Main()
-
     {
-        //Scripture class, remember to put on separate file//
-        Scripture scripture = new Scripture ("John 3:16: For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-        
+
+        Scripture scripture = Scripture ("John 3:16", "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+
         Console.Clear();
         scripture.DisplayScripture();
 
         while (true)
         {
-            Console.WriteLine ("Press Enter Key to hide a Scripture word or type quit to exit the program: ");
-            
+            Console.WriteLine("Press enter to hide a Scripture word or type quit to exit the program:");
             string userInput = Console.ReadLine();
-            
+
+
             if (userInput.ToLower() == "Quit")
-            
             {
                 break;
             }
-
             else
             {
-               Console.Clear ();
-               scripture.HideWord ();
-               scripture.DisplayScripture ();
+                
+                Console.Clear();
+                scripture.HideWord();
+                scripture.DisplayScripture();
 
-               if (scripture.AllWordsHidden())
+                
+                if (scripture.AllWordsHidden())
                 {
-                 Console.WriteLine ("Congratulations, you memorized the scripture!");
-            
-                 break;
-
+                    Console.WriteLine("Congratulations, you memorized the scripture!");
+                    break;
                 }
-
             }
-        } 
+        }
+    }
+
+    private static Scripture Scripture(string v1, string v2)
+    {
+        throw new NotImplementedException();
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,70 +1,51 @@
 using System;
-class Scripture 
+using System.Collections.Generic;
+using System.Linq;
+
+class Scripture
 {
     private string reference;
     private string text;
+    private List<string> hiddenWords;
 
-    private List <string> hiddenWords;
-    private string v;
-
-    public Scripture (string reference, string text)
-
+    public Scripture(string reference, string text)
     {
         this.reference = reference;
         this.text = text;
-        this.hiddenWords = new List<string> ();
-        
+        this.hiddenWords = new List<string>();
     }
 
-    public Scripture(string v)
+    public void DisplayScripture()
     {
-        this.v = v;
+        Console.WriteLine(reference);
+        string[] words = text.Split(' ');
+        foreach (string word in words)
+        {
+            if (hiddenWords.Contains(word))
+            {
+                Console.Write("_____ ");
+            }
+            else
+            {
+                Console.Write(word + " ");
+            }
+        }
+        Console.WriteLine();
     }
 
-    public void DisplayScripture ()
-
+    public void HideWord()
     {
-        Console.WriteLine (reference);
-            string [] words = text.Split ();
-             foreach (string word in words)
-                {
-                    if (hiddenWords.Contains(word))
-                    {
-                        Console.Write ("____");
-
-                    }
-
-                        else
-
-                        {
-                         Console.Write(word +"");
-
-                        }
-
-                }
-
-        Console.WriteLine ();
-    }
-
-    public void HideWord ()
-    {
-
-        string [] words = text.Split ();
-        Random rand = new Random ();
-
-        int index = rand.Next (words.Length);
-        string wordToHide = words [index];
+        string[] words = text.Split(' ');
+        Random rand = new Random();
+        int index = rand.Next(words.Length);
+        string wordToHide = words[index];
 
         hiddenWords.Add(wordToHide);
     }
 
-    public bool AllWordsHidden ()
-
+    public bool AllWordsHidden()
     {
-        string [] words = text.Split ();
-
+        string[] words = text.Split(' ');
         return hiddenWords.Count == words.Length;
-
     }
-
 }
