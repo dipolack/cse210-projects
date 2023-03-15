718b2fcd3d21c53821975e9023c9824ecd7e1506
dipolack <pol22007@byui.edu>
2023-03-14 20:31:07 -0400

    adding private attributes and constructors

---
+++ prove/Develop05/goal.cs
@@ -0,0 +1,30 @@
+public class Goal
+
+{
+    // Private attributes
+
+    public string name;
+
+    public string getName;
+
+    public int RecordGoal;
+
+    public int Serialize;
+
+    public static Goal Deserialize;
+
+    public int points;
+
+    public bool completed;
+
+
+    // Constructor
+    public Goal(string name, int points)
+
+    {
+        this.name = name;
+
+        this.points = points;
+
+        this.completed = false;
+    }
