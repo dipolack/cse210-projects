a51db2b5af159e683572215c6d5f00ec41db77fb
dipolack <pol22007@byui.edu>
2023-03-14 20:20:17 -0400

    working on goaltracker and constructors

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,75 @@
 using System;
 
-class Program
+using System.Collections.Generic;
+
+//Class for tracking the user's goals and score
+
+public class GoalTracker
+
 {
-    static void Main(string[] args)
+
+    //Private attributes
+    private List<Goal> goals;
+
+    private int score;
+
+    //Constructor
+
+    public GoalTracker()
+
     {
-        Console.WriteLine("Hello Develop05 World!");
+        goals = new List<Goal>();
+
+        score = 0;
     }
-}
+
+    //Add a new goal to the list of goals
+
+    public void AddGoal(Goal goal)
+
+    {
+        goals.Add(goal);
+    }
+
+//Mark a goal as completed
+
+    public class LargeGoal : Goal
+
+    {
+        private int progress;
+
+        private int target;
+
+        public LargeGoal(string name, int points, int target) : base(name, points)
+
+        {
+            this.target = target;
+
+            this.progress = 0;
+        }
+
+
+        public void AddProgress(int amount)
+
+        {
+            this.progress += amount;
+
+            if (this.progress >= this.target)
+
+            {
+               this.completed = true;
+
+                this.points += 500;
+            }
+
+        }
+
+         public override string GetDescription()
+
+        {
+            string status = this.completed ? "Completed" : "Incomplete";
+
+            return $"{this.name}: {status}, Progress: {this.progress}/{this.target}";
+        }
+
+    }
