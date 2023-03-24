a86b1abcd9ad6320ff2e7542e697a7b4d4fe7b1e
dipolack <pol22007@byui.edu>
2023-03-24 14:44:54 -0400

    adding _

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,24 +6,24 @@ class Program
     {
         static void Main(string[] args)
         {
-            List<Video> videos = new List<Video>();
+            List<Video> _videos = new List<Video>();
 
             Video v1 = new Video("C# Learning about Abstraction", "James Bond", 300);
             v1.Comments.Add(new Comment("Leah", "Wonderful Concepts!"));
             v1.Comments.Add(new Comment("Martha", "Insightful content!"));
             v1.Comments.Add(new Comment("Carl", "I learned a lot about Abstraction, thank you!"));
-            videos.Add(v1);
+            _videos.Add(v1);
 
             Video v2 = new Video("C# Learning about Inheritance", "John Wick", 450);
             v2.Comments.Add(new Comment("Maria", "I will make sure to share this info!"));
             v2.Comments.Add(new Comment("Cristopher", "Very detailed and informative!"));
-            videos.Add(v2);
+            _videos.Add(v2);
 
             Video v3 = new Video("C# Learning about Polymorphism", "Rocky Valvoa", 200);
             v3.Comments.Add(new Comment("Leon", "I loved learning the difference between Inheritance and Polymorphism!"));
-            videos.Add(v3);
+            _videos.Add(v3);
 
-            foreach (Video v in videos)
+            foreach (Video v in _videos)
             {
                 Console.WriteLine("Title: " + v.Title);
                 Console.WriteLine("Author: " + v.Author);
