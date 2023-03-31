ea97711b18cb87f8e4b0e68a13ec46010313dbef
dipolack <pol22007@byui.edu>
2023-03-30 20:23:28 -0400

    changing date

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -12,15 +12,15 @@ class Program
 
         {
             
-            new Running(new DateTime(2022, 3, 1), 30, 4.2),
-            new StationaryBicycle(new DateTime(2022, 3, 2), 45, 30),
-            new Swimming(new DateTime(2022, 3, 3), 60, 20),
-            new Running(new DateTime(2022, 3, 4), 45, 6.1),
-            new StationaryBicycle(new DateTime(2022, 3, 5), 60, 25),
-            new Swimming(new DateTime(2022, 3, 6), 90, 40),
-            new Running(new DateTime(2022, 3, 7), 30, 3.5),
-            new StationaryBicycle(new DateTime(2022, 3, 8), 60, 35),
-            new Swimming(new DateTime(2022, 3, 9), 45, 30)
+            new Running(new DateTime(2023, 3, 1), 30, 4.2),
+            new StationaryBicycle(new DateTime(2023, 3, 2), 45, 30),
+            new Swimming(new DateTime(2023, 3, 3), 60, 20),
+            new Running(new DateTime(2023, 3, 4), 45, 6.1),
+            new StationaryBicycle(new DateTime(2023, 3, 5), 60, 25),
+            new Swimming(new DateTime(2023, 3, 6), 90, 40),
+            new Running(new DateTime(2023, 3, 7), 30, 3.5),
+            new StationaryBicycle(new DateTime(2023, 3, 8), 60, 35),
+            new Swimming(new DateTime(2023, 3, 9), 45, 30)
         
         };
 
